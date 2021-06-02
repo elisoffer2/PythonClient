a5ff2110548cfdf0ccccc6e813b3b833757f5d8d
Eli Soffer <elisoffer2@gmail.com>
2021-06-01 17:47:46 -0700

    socketio framework established

--- OthelloClient.py
+++ OthelloClient.py
@@ -3,20 +3,65 @@ import sys
 import getopt
 
 sio = socketio.Client()
+gOpponentName = ''
 
 @sio.event
 def connect():
-    print('connection established')
+    print("I'm connected!")
+    data = {}
+    data['name'] = "TeamName"
+    sio.emit("set team", data)
+    print("Team Name Emitted")
 
-@sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+@sio.on('set timeout')
+def set_timeout(data):
+    moveTimeout = data['timeout']
+    print("moveTimeout set to " + str(moveTimeout) + " Seconds")
+
+@sio.on('set opponent')
+def set_opponent(data):
+    gOpponentName = data["name"]
+    gameID = data["game_id"]
+    print(data["game_id"])
+    print(data["name"])
+    print("Game ID set to " + gameID + " with opponent " + gOpponentName);
+
+@sio.on('make move')
+def make_move(data):
+    gameID = data["game_id"]
+    boardStr = data["board"]
+    turnStr = data["turn"]
+    print(gameID)
+    print(boardStr)
+    print(turnStr)
+
+    # This is where we need to implement the othello class to know what to do
+    data["board"] = "0100002101002021222221112222220121212221212122212201020120011111"
+    # end othello class with the next move (can I make a python wrapper?)
+
+    sio.emit("move", data)
+
+
+@sio.on('game ended')
+def game_ended(data):
+        gameID = data["game_id"]
+        blackCount = data["black_count"]
+        white_count = data["white_count"]
+        print("Game ended: B-" + str(black_count) + " W-" + str(white_count));
+
+@sio.on('tournament ended')
+def tournament_ended(data):
+    pname = data["name"]
+    game_count = data["game_count"]
+    win_count = data["win_count"]
+    tie_count = data["tie_count"]
+    print("Tournament results for " + pname + " Games: " + str(game_count))
+    print(" Wins: " + str(win_count) + " Ties: " + str(tie_count))
 
 @sio.event
 def disconnect():
     print('disconnected from server')
-
+    sys.exit()
 
 def main(argv):
     teamName = ''
@@ -39,7 +84,7 @@ def main(argv):
     print("IP Address is", serverIP)
     arguementCount = len(sys.argv)
     arguementStrings = str(sys.argv)
-    print("http://" + serverIP + ":8080")
+    print("connecting to http://" + serverIP + ":8080")
 
     sio.connect("http://" + serverIP + ":8080")
     sio.wait()
